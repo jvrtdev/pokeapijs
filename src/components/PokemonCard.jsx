e1ae50158854138c83348d0747137f2569beb3db
João <joaovitorromanoteixeira@gmail.com>
2023-12-20 11:05:01 -0300

    :boom: fix img src

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,13 +7,13 @@ export default function PokemonCard({data}) {
         
     
     return (
-        <div className={`${data.types[0].type.name} m-5 p-2 w-[200px] rounded-xl shadow-md cursor-pointer`}>
+        <div className={`${data.types[0].type.name} m-5 p-2 w-[200px] h- rounded-xl shadow-md cursor-pointer`}>
             <div className='flex justify-between items-cente mb-1'>
                 <p className="font-bold text-center text-white capitalized text-2xl">{data.name}</p>
                 <p className=" font-semibold text-center text-white text-2xl">#{data.id}</p>
             </div>
                 <div className="bg-ellipse bg-center bg-no-repeat bg-contain">
-                    <img src={data.sprites.front_default} alt={data.name} 
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt={data.name} 
                     className="w-full"
                     />
                 </div>
