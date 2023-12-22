9302aa71ee079c7971e8f2a81d9cf3106abed59e
David <david@MacBook-Pro-de-David.local>
2023-12-21 22:06:28 -0300

    :sparkles: feat: about.js with dynamicRoute

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
 
 
 export default function About() {
   const { pokeName } = useParams()
-  const currentPokemon = pokemons.filter(pokemon => pokemon.id === pokeName)
+  const [ pokemon, setPokemon ] = useState([])
+  console.log(pokemon)
+  
+  
+    const getPokemonDetail = async () => {
+      try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+      const { stats, abilities, id, name, types } = response.data
+
+      const currentType = types[0].type.name
+
+      setPokemon([{
+        stats,
+        abilities,
+        id,
+        name,
+        types,
+      }])
+      
+    }
+    catch (err) {
+      alert('Ocorreu um erro')
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+  getPokemonDetail()
+  },[])
 
   return (
     <div className=''>
-      {pokeName}
-      {currentPokemon.name}
+      
+      
+      {pokemon.map((item, index) => (
+        <div
+        key={index}
+        >
+          {item.name}
+        </div>
+      ))}
       
     </div>
   )
